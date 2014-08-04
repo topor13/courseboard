c070019cffbd108215f38edf6e1f061fe346a1ae
doniv <donivrecord@gmail.com>
2014-08-04 18:24:26 +0800

    New app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Courseboard::Application.config.secret_key_base = 'bde29ab27d02892b525a300f82a0422902c083d263d552f25e312348886960bf02fd96032fafded4ed19a298b50ec15b1ca7ca917e0568adf8641c4f66ef9886'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Courseboard::Application.config.session_store :cookie_store, key: '_courseboard_session'
